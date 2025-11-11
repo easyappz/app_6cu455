7074f7809e7d7dcaf3173ff89da67a642267f24e
IbragimKhaniev <newinterny@gmail.com>
2025-11-11 10:03:36 +0300

    Register Profile model in Django admin with list_display and search_fields

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'phone', 'full_name', 'created_at')
+    search_fields = ('user__username', 'user__email', 'phone', 'full_name')
